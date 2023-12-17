c30ed2c527ef78bd078dd7aae495ef45ef457c8a
groovedealers1 <153697448+groovedealers1@users.noreply.github.com>
2023-12-17 14:12:14 +1100

    Update stite.py

--- stite.py
+++ stite.py
@@ -1,13 +1,18 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 
 
 app = Flask(__name__, template_folder='template')
 
 
-@app.route('/')
+@app.route('/index/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('home_page.html')
+    if request.method == 'POST':
+        return render_template('jump.html')
+    else:
+        return render_template('home_page.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
